fb7a336b652c5f5f89341cdae8c0b87c5b32f754
sushildevloper <71219832+sushildevloper@users.noreply.github.com>
2026-07-10 14:52:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,8 @@
 
+resource "aws_s3_bucket" "my-example" {
+  # Note: Standard S3 bucket names must be globally unique across all of AWS
+  bucket = "sushil-unique-test-bucket-2026-${var.env_tag}"
+  tags = {
+    description = "bucket creation"
+  }
+}
